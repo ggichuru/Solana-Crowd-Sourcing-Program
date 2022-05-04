b3546e6ecb9f07c64dd300d169523bfe76cc7816
ggichuru <ggichuru.dev@gmail.com>
2022-05-04 17:23:41 +0300

    [fn] create_campaign

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ entrypoint!(process_instruction);
 
 
   // ------------------------------//
- //          CREATE CAMPAIGN      //
+ //         CREATE CAMPAIGN  (0)  //
 //-------------------------------//
 
 // Campaign Details struct
@@ -72,17 +72,69 @@ struct CampaignDetails {
 }
 
 
+// Create Campaign function
 fn create_campaign(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8]) -> ProgramResult {
+        // Create an iterator on accounts
+        let accounts_iter = &mut accounts.iter();
+
+        // our writing account (program_account)
+        let writing_account = next_account_info(accounts_iter)?;
+
+        // account of the person creating the campaign
+        let creater_account = next_account_info(accounts_iter)?;
+
+        // Make sure the account signing this tx is the creator_account
+        if !creater_account.is_signer {
+            msg!("Creator Account should be the signer");
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        //Make sure this account is owned by the program
+        if writing_account.owner != program_id {
+            msg!("Writting Account should be owned by program");
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        
+        // Get input_data
+        let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+            .expect("Failed to deserialize input data");
+        
+        
+        // Make sure only acdmin created a campaign
+        if input_data.admin != *creater_account.key {
+            msg!("Invalid instruction data");
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+
+        // Get the minimum balance we need in out program account
+        let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+
+        // Make sure the account has enough balance(lamports) to create a campaign
+        if **writing_account.lamports.borrow() < rent_exemption {
+            msg!("Not enough balance. Should be more than the rent_exempt");
+            return Err(ProgramError::InsufficientFunds);
+        }
+
+
+        // Set the initial amount donated to 0
+        input_data.amount_donated = 0;
+
+
+        // Serialize the input_data to write data in a writing account (program account)
+        input_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
+
         Ok(())
     }
 
 
 
   // ------------------------------//
- //          DONATE               //
+ //          DONATE (1)           //
 //-------------------------------//
 
 fn donate(
@@ -95,7 +147,7 @@ fn donate(
 
 
   // ------------------------------//
- //          WITHDRAW             //
+ //          WITHDRAW (2)         //
 //-------------------------------//
 
 fn withdraw(
