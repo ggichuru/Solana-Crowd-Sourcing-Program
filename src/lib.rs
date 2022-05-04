b977f9b4a47366d472ceb2c3ec555573693d41ed
ggichuru <ggichuru.dev@gmail.com>
2022-05-04 16:00:56 +0300

    Entrypoint function

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,76 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult {
-    Ok(())
-}
+    // Makes sure there's instruction data
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    
+    if instruction_data[0] == 0 {
+        return create_campaign(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()]
+        )
+    } else if instruction_data[0] == 1 {
+        return withdraw(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()]
+        )
+    } else if instruction_data[0] == 2 {
+        return donate(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()]
+        )
+    }
+
+    // Throw error if instruction_data doesn't match
+    msg!("Didn't find the required entrypoint");
+    Err(ProgramError::InvalidInstructionData)
+} 
+
 
 entrypoint!(process_instruction);
+
+
+
+
+  // ------------------------------//
+ //          CREATE CAMPAIGN      //
+//-------------------------------//
+
+fn create_campaign(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]) -> ProgramResult {
+        Ok(())
+    }
+
+
+
+  // ------------------------------//
+ //          DONATE               //
+//-------------------------------//
+
+fn donate(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8]) -> ProgramResult {
+            Ok(())
+}
+
+
+
+  // ------------------------------//
+ //          WITHDRAW             //
+//-------------------------------//
+
+fn withdraw(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]) -> ProgramResult {
+        Ok(())
+}
